c6a7eb57b94c4411469fc0edda7a3c2a7ecfc239
Muhammad Talha <talha@Muhammads-MacBook-Pro.local>
2018-10-07 16:46:24 +0200

    k

--- web-bluetooth/notifications-async-await.js
+++ web-bluetooth/notifications-async-await.js
@@ -62,11 +62,14 @@ async function onStopButtonClick() {
 function handleNotifications(event) {
   let value = event.target.value;
   let a = [];
+  let b = [];
   // Convert raw data bytes to hex values just for the sake of showing something.
   // In the "real" world, you'd use data.getUint8, data.getUint16 or even
   // TextDecoder to process raw data bytes.
   for (let i = 0; i < value.byteLength; i++) {
     a.push('0x' + ('00' + value.getUint8(i).toString(16)).slice(-2));
+    b.push('00' + value.getUint8(i).toString(10)).slice(-2);
   }
   log('>' + a.join(' '));
+  log('>' + b.join(' '));
 }
