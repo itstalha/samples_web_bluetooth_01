faa9d9e4fab35d88c7953067444b5e8385164b49
Talha <mohammad_talha@hotmail.com>
2018-10-09 23:01:23 +0200

    ok

--- web-bluetooth/notifications-async-await.js
+++ web-bluetooth/notifications-async-await.js
@@ -2,7 +2,7 @@ var myCharacteristic;
 
 async function onStartButtonClick() {
   dataFromSensor.push(Math.random());
-  log('Commits: '+ 11);
+  log('Commits: '+ 12);
   let serviceUuid = document.querySelector('#service').value;
   log('0. serviceUuid > '+serviceUuid);
   if (serviceUuid.startsWith('0x')) {
