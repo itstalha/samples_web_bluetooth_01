b462eb722d9195c426ccc5221892b898caf78ebe
Talha <mohammad_talha@hotmail.com>
2018-10-11 02:20:25 +0200

    ok

---
+++ web-bluetooth/app/js/loadcell.js
@@ -0,0 +1,106 @@
+var myCharacteristic;
+
+async function onStartButtonClick() {
+  dataFromSensor.push(Math.random());
+  ////log('Commits: '+ 18);
+
+
+  //Talha - Logging Service UUID
+ let  serviceUuid = '0000ffe0-0000-1000-8000-00805f9b34fb';
+////  log('2. serviceUuid > '+serviceUuid);
+
+  //Talha - Logging characteristicUuid 
+  let   characteristicUuid = '0000ffe1-0000-1000-8000-00805f9b34fb';
+////  log('2. characteristicUuid > '+characteristicUuid);
+
+  try {
+   //// log('Requesting Bluetooth Device...');
+    const device = await navigator.bluetooth.requestDevice({
+        filters: [{services: [serviceUuid]}]});
+
+  ////  log('Connecting to GATT Server...');
+    const server = await device.gatt.connect();
+
+ ////   log('Getting Service...');
+    const service = await server.getPrimaryService(serviceUuid);
+
+   //// log('Getting Characteristic...');
+    myCharacteristic = await service.getCharacteristic(characteristicUuid);
+
+    await myCharacteristic.startNotifications();
+
+   //// log('> Notifications started');
+    myCharacteristic.addEventListener('characteristicvaluechanged',
+        handleNotifications);
+  } catch(error) {
+   //// log('Argh! ' + error);
+  }
+}
+
+async function onStopButtonClick() {
+  if (myCharacteristic) {
+    try {
+      await myCharacteristic.stopNotifications();
+     //// log('> Notifications stopped');
+      myCharacteristic.removeEventListener('characteristicvaluechanged',
+          handleNotifications);
+    } catch(error) {
+    ////  log('Argh! ' + error);
+    }
+  }
+}
+
+function handleNotifications(event) {
+  let value = event.target.value;
+  //let a = [];
+  //let b = [];
+  let c = [];
+  let d = [];
+  // Convert raw data bytes to hex values just for the sake of showing something.
+  // In the "real" world, you'd use data.getUint8, data.getUint16 or even
+  // TextDecoder to process raw data bytes.
+ // log('value>' + value);
+ // log('value.toString()>' + value.toString());
+ var temp = '';
+ var floatNumber = '';
+
+  for (let i = 0; i < value.byteLength; i++) {
+
+
+  //  a.push('0x' + ('00' + value.getUint8(i).toString(16)).slice(-2));
+  //  b.push(('00' + value.getUint8(i).toString(10)).slice(-2));
+    
+    temp =String.fromCharCode(value.getUint8(i));
+    c.push(temp);
+    if ((temp != 's') && (temp != 'e'))
+    {
+      floatNumber = floatNumber + temp;
+      d.push(temp);
+      
+    }
+    
+   // dataFromSensor.push(('00' + value.getUint8(i).toString(10)).slice(-2));
+     
+  }
+  //log('a>' + a.join(' '));
+  //log('b>' + b.join(' '));
+  
+  //var str = "- 0 . 0 5";
+  //str = str.replace(/\s+/g, '');
+
+  floatNumber = floatNumber.replace(/\s+/g, '');
+
+  
+  ////log('c> ' + c.join(' '));
+ //// log('d> ' + d.join(' '));
+  ////log('floatnumber> ' + floatNumber );
+  if (floatNumber < 0)
+  {
+    floatNumber = 0;
+  }
+  dataFromSensor.push(floatNumber);
+  document.getElementById("liveForce").innerHTML = floatNumber;
+  //d = d.map(Number);
+  //dataFromSensor.push(d);
+  //log('arr> ' + d);
+}
